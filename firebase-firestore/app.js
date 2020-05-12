db0f57912d3e648449fc34acaac9f826ce17b990
waynegakuo <waynejotham@gmail.com>
2020-05-12 22:35:50 +0300

    Saving data to Firestore

--- firebase-firestore/app.js
+++ firebase-firestore/app.js
@@ -1,6 +1,8 @@
 const cafeList = document.querySelector('#cafe-list');
 let output = '';
 
+const form = document.querySelector('#add-cafe-form');
+
 // Getting all documents in collection cafes
 db.collection('cafes').get()
     .then((snapshot) => {
@@ -16,6 +18,18 @@ db.collection('cafes').get()
         cafeList.innerHTML = output
     });
 
+// Saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = ''
+    form.city.value = ''
+})
+
+
 
 /** 1. Using DOM manipulation to append info from Firebase
  * //  Create elements and render cafe
@@ -23,15 +37,25 @@ db.collection('cafes').get()
         let li = document.createElement('li');
         let name = document.createElement('span')
         let city = document.createElement('span')
+        let cross = document.createElement('div)
 
         li.setAttribute('data-id', doc.id);
         name.textContent = doc.data().name;
         city.textContent = doc.data().city;
+        cross.textContent = 'x
 
         li.appendChild(name);
         li.appendChild(city);
+        li.appendChild(cross);
 
         cafeList.appendChild(li);
+
+        // Deleting data
+        cross.addEventListener('click', (e) => {
+            e.stopPropagation();
+            let id = e.target.parentElement.getAttribute('data-id');
+            db.collection('cafes').doc(id).delete();
+        })
     }
  */
 
